4da1432ada7f4aaf93644aac4d8c3a5ee975ca7f
Oğuz <oguzkaratoruk@yandex.com>
2019-01-12 19:30:11 +0300

    Initial Commit

---
+++ SimpleSideMenu/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SimpleSideMenu
+//
+//  Created by Oğuz Karatoruk on 12.01.2019.
+//  Copyright © 2019 Oğuz Karatoruk. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
